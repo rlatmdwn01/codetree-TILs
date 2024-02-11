4e7280c0e7949f622b37d818b95a99a38f5db7fd
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-11 21:24:20 +0900

    Title: 점수 비교, Time: 135ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240211/점수 비교/score-comparison.py
+++ 240211/점수 비교/score-comparison.py
@@ -1,5 +1,5 @@
-math_A,math_B=map(int,input().split())
-eng_A,eng_B=map(int,input().split())
+math_A,eng_A=map(int,input().split())
+math_B,eng_B=map(int,input().split())
 
 if math_A>=math_B and eng_A>=eng_B:
     print(1)
