a8037171ca46cc4f3559ca55ab932d0d6f05c592
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-15 20:34:03 +0900

    Title: 윤년인가, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240215/윤년인가/is-leap-year.py
@@ -0,0 +1,11 @@
+y=int(input())
+
+if y%4==0:
+    print('true')
+    if y%100==0:
+        print('false')
+    elif y%4==0 and y%100==0 and y%400==0:
+        print('true')
+
+else:
+    print('false')
