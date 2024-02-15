60c553a311ff2ef96d5f0ebb1c42d22b6f168861
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-15 20:40:39 +0900

    Title: 일 수 구하기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240215/일 수 구하기/number-of-days-in-month.py
+++ 240215/일 수 구하기/number-of-days-in-month.py
@@ -5,15 +5,15 @@ if n==1:
 elif n==2:
     print(28)
 elif n==3:
-    print(30)
+    print(31)
 elif n==4:
-    print(31)
+    print(30)
 elif n==5:
-    print(30)
-elif n==6:
     print(31)
-elif n==7:
+elif n==6:
     print(30)
+elif n==7:
+    print(31)
 elif n==8:
     print(31)
 elif n==9:
