c10e36c46438cc2eac76603dc81f4736db1d206c
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-17 23:06:24 +0900

    Title: a 부터 b 까지, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240217/a 부터 b 까지/a-to-b.py
+++ 240217/a 부터 b 까지/a-to-b.py
@@ -2,13 +2,11 @@ a,b=input().split()
 
 a=int(a)
 b=int(b)
-print(a,end=' ')
+i=a
 
-for i in range(a,b+1):
-    while a<b:
-        if a%2==1:
-            a=a*2
-        else:
-            a=a+3
-
-        print(a,end=' ')
+while i<=b:
+    print(i, end=" ")
+    if i%2==1:
+        i=i*2
+    else:
+        i=i+3
