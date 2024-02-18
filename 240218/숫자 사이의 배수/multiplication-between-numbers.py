d57f3b1fe09eb47a2a644c116f0004fa04c65b16
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-18 19:52:17 +0900

    Title: 숫자 사이의 배수, Time: 154ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240218/숫자 사이의 배수/multiplication-between-numbers.py
@@ -0,0 +1,14 @@
+a,b=input().split()
+
+a=int(a)
+b=int(b)
+
+sum_val=0
+cnt=0
+
+for i in range(a,b+1):
+    if i%5==0 or i%7==0:
+        sum_val+=i
+        cnt+=1
+
+print(sum_val,"%.1f"%((sum_val)/cnt))
