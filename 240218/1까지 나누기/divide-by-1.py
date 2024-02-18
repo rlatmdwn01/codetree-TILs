271c63aa05985f9cca7ea9b8008ce4c5605c23da
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-18 21:52:16 +0900

    Title: 1까지 나누기, Time: 125ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/1까지 나누기/divide-by-1.py
+++ 240218/1까지 나누기/divide-by-1.py
@@ -1,11 +1,11 @@
 n=int(input())
-cnt=1
+cnt=0
 m=n
 
 for i in range(1,n+1):
-    if m//cnt>1:
+    if m//i>1:
         cnt+=1
-        m=m//cnt
+        m=m//i
     else:
         break
 print(cnt+1)
