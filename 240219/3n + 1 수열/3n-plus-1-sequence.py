eac5573c9d7e49dae50edb863b2115739d24664e
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-19 07:58:19 +0900

    Title: 3n + 1 수열, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240219/3n + 1 수열/3n-plus-1-sequence.py
@@ -0,0 +1,15 @@
+n=int(input())
+cnt=0
+
+while True:
+    if n!=1:
+        if n%2==0:
+            n=n//2
+            cnt+=1
+        else:
+            n=n*3+1
+            cnt+=1
+    else:
+        break
+
+print(cnt)
