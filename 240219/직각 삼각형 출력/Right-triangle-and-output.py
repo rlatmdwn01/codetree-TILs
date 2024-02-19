8018d9860b8fbb9ec3ded3fa260437fb9a125b14
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-19 22:32:00 +0900

    Title: 직각 삼각형 출력, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240219/직각 삼각형 출력/Right-triangle-and-output.py
+++ 240219/직각 삼각형 출력/Right-triangle-and-output.py
@@ -1,6 +1,6 @@
 n=int(input())
 
-for i in range(1,n+1):
-    for j in range(1,i*2):
-        print('*',end="")
+for i in range(n):
+    for _ in range(2*i+1):
+        print("*",end="")
     print()
