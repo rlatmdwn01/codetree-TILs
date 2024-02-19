828cc1e2d86acf6915b0c481b2134dd74463aee9
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-19 18:13:13 +0900

    Title: ab 사이에 있는 c, Time: 130ms, Memory: 73MB, Status: Passed - CodeTree

---
+++ 240219/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,15 @@
+a,b,c=input().split()
+a=int(a)
+b=int(b)
+c=int(c)
+
+satisfied=False
+
+for i in range(a,b+1):
+    if i%c==0:
+        satisfied=True
+
+if satisfied==True:
+    print('YES')
+else:
+    print('NO')
