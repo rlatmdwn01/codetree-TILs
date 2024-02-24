94077586384b4fed981bb5b799fad39020bdca1c
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-24 23:57:02 +0900

    Title: 100 도달하기, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/100 도달하기/reach-100.py
+++ 240224/100 도달하기/reach-100.py
@@ -2,11 +2,10 @@ n=int(input())
 
 num_list=[1,n]
 
-print(1, end=' ')
-print(n, end=' ')
-
-for i in range(2,11):
+for i in range(2,21):
     num_list.append(num_list[-2]+num_list[-1])
-    print(num_list[-1], end=' ')
-    if num_list[-1]>100:
+
+for i in num_list:
+    print(i, end=' ')
+    if i>100:
         break
