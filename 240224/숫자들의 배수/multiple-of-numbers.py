c7e13f089b382cef3558894fe0cc43a2295c467c
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-24 23:41:53 +0900

    Title: 숫자들의 배수, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/숫자들의 배수/multiple-of-numbers.py
+++ 240224/숫자들의 배수/multiple-of-numbers.py
@@ -1,6 +1,11 @@
 n=int(input())
 
 num_list=[i*n for i in range(1,11)]
+cnt=0
 
 for i in num_list:
-    print(i, end=' ')
+    print(i,end=' ')
+    if i%5==0:
+        cnt+=1
+    if cnt==2:
+        break
