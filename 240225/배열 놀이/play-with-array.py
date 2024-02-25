36ab0d15938f8f0a6dae6b6fe787fe207f43e5a9
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-25 23:11:16 +0900

    Title: 배열 놀이, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 240225/배열 놀이/play-with-array.py
+++ 240225/배열 놀이/play-with-array.py
@@ -1,37 +1,20 @@
-# n개 원소
-# q개 질의
-# 1번 질의 a : a번째 원소 출력(배열 n)
-# 2번 질의 a : 숫자 a가 있는지 판단 -> 있을 경우 몇 번 째 원소? -> 없을 경우 0 출력
-# 3번 질의 a b : a번째 원소부터 b번째 원소까지 순서대로 출력
-
-# 첫 줄 : n(배열길이) q
-# 둘째줄 : n개의 원소
-# 셋째 줄 : q개의 질의
-
 n, q = map(int, input().split())
-arr = list(map(int, input().split()))
 
-# n = 3
-# q = 1
-# arr = [1, 8, 5]
-for i in range(q) :
-    questions = list(map(int, input().split()))
-    #questions = [1, 1]
-    q_type= questions[0]
-    a = questions[1]
+num_list=list(map(int,input().split()))
 
-    if q_type == 1 :
-        if a <= len(arr) :
-            print(arr[a - 1])
-        else :
+for _ in range(q):
+    q_list=list(map(int,input().split()))
+
+    if q_list[0]==1:
+        print(num_list[q_list[1]-1])
+
+    elif q_list[0]==2:
+        if q_list[1] in num_list:
+            print(num_list.index(q_list[1])+1)
+        else:
             print(0)
-    elif q_type == 2 :
-        if a in arr :
-            print(arr.index(a) + 1)
-        else :
-            print(0)
-    else : 
-        b = questions[2]
-        for j in range(a-1, b) :
-            print(arr[j], end = ' ')
-            print()
+    elif q_list[0]==3:
+        for j in range(q_list[1],q_list[2]+1):
+            print(num_list[j-1],end=' ')
+
+        print()
