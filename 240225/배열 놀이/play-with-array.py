83b5f416c504058e0717a2de1564f280c4e842f0
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-25 22:49:50 +0900

    Title: 배열 놀이, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240225/배열 놀이/play-with-array.py
+++ 240225/배열 놀이/play-with-array.py
@@ -1,21 +1,19 @@
-n,q=input().split()
-n=int(n)
-q=int(q)
+n, q = map(int, input().split())
 
-num_list=list(map(int,input().split()))
+dnjsth = list(map(int, input().split()))
 
-for i in range (q):
-    q_list=list(map(int,input().split()))
-
-
-    if q_list[0]==1:
-        print(num_list[(q_list[1])-1])
-    elif q_list[0]==2:
-        if (q_list[1]) in num_list:
-            print(num_list.index((q_list[1]))+1)
+for _ in range(q):
+    lst = list(map(int, input().split()))
+    if lst[0] == 1:
+        print(dnjsth[lst[1]-1])
+    elif lst[0] == 2:
+        if lst[1] in dnjsth:
+            for j in dnjsth:
+                if j == lst[1]:
+                    print(dnjsth.index(j)+1)
+                    break
         else:
-            print(0)
-
-    elif q_list[0]==3:
-        for j in range((q_list[1]),(q_list[2])+1):
-            print(num_list[j-1], end=' ')
+            print('0')
+    elif lst[0] == 3:
+        for i in range(lst[1]-1, lst[2]):
+            print(dnjsth[i], end=' ')
