dd908be6a5ed3a49d38d05ef0cd7db552e9e2884
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-26 22:42:32 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240226/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
+++ 240226/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.py
@@ -1,23 +1,13 @@
-n = int(input())
-arr = list(map(int, input().split()))
+n=int(input())
+price=list(map(int,input().splti()))
 
-prev_min_idx = 0 
-min_idx = 0
+max_profit=0
+for i in range(n):
+    for j in range(i+1,n):
+        profit=price[j]-price[i]
 
-for i in range(prev_min_idx+1, n):
-    if arr[i] < arr[min_idx]:
-        min_idx = i
+        if profit>max_profit:
+            max_profit=profit
 
-    prev_min_idx = min_idx
 
-max_idx = min_idx
-for j in range(prev_min_idx+1, n):
-    if arr[max_idx] < arr[j]:
-        max_idx = j
-    
-    prev_min_idx = max_idx
-
-if min_idx == n - 1:
-    print("0")
-else:
-    print(arr[max_idx] - arr[min_idx])
+print(max_profit)
