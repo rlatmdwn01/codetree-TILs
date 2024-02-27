92205064d22fcb54e4484b0ee366d86dea92177b
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-27 14:40:25 +0900

    Title: 두 개의 격자 비교하기, Time: 299ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/두 개의 격자 비교하기/compare-two-grid.py
+++ 240227/두 개의 격자 비교하기/compare-two-grid.py
@@ -3,8 +3,8 @@ n,m=input().split()
 n=int(n)
 m=int(m)
 
-arr_1=[list(map(int,input().split())) for _ in range (m)]
-arr_2=[list(map(int,input().split())) for _ in range (m)]
+arr_1=[list(map(int,input().split())) for _ in range (n)]
+arr_2=[list(map(int,input().split())) for _ in range (n)]
 
 for i in range(n):
     for j in range(m):
