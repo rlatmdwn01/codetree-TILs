6a5262469b272b93d7b038b3001446f5801ff4f8
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-27 11:11:30 +0900

    Title: 배열의 평균, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240227/배열의 평균/ave-of-array.py
+++ 240227/배열의 평균/ave-of-array.py
@@ -1,5 +1,5 @@
 arr2d=[
-    list(input().split())
+    list(map(int,input().split()))
     for _ in range(2)
 ]
 
@@ -8,7 +8,7 @@ for i in range(2):
     sum_val=0
     for j in range(4):
         sum_val+=int(arr2d[i][j])
-    print((sum_val/4),end=' ')
+    print("%.1f"%(sum_val/4),end=' ')
 
 print()
 
@@ -17,7 +17,7 @@ for i in range(4):
     sum_val=0
     for j in range(2):
         sum_val+=int(arr2d[j][i])
-    print((sum_val)/2,end=' ')
+    print("%.1f"%(sum_val/2),end=' ')
 
 print()
 
@@ -26,4 +26,4 @@ sum_val=0
 for i in range(4):
     for j in range(2):
         sum_val+=int(arr2d[j][i])
-print(sum_val/8)
+print("%.1f"%(sum_val/8))
