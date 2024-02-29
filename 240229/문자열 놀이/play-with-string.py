5a28bf58beae0d2d74bfc1a50094393c84952815
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-02-29 16:56:19 +0900

    Title: 문자열 놀이, Time: 169ms, Memory: 28MB, Status: Passed - CodeTree

--- 240229/문자열 놀이/play-with-string.py
+++ 240229/문자열 놀이/play-with-string.py
@@ -1,5 +1,6 @@
 s,q=input().split()
 q=int(q)
+s_list=list(s)
 
 for i in range(q):
     a,b,c=input().split()
@@ -8,15 +9,13 @@ for i in range(q):
     if a==1:
         b=int(b)
         c=int(c)
-        s_list=list(s)
         t=s_list[b-1]
         s_list[b-1]=s_list[c-1]
         s_list[c-1]=t
         print(''.join(s_list))
 
     elif a==2:
-        s_list=list(s)
-        for i in s_list:
-            if i==b:
-                i=c
+        for i in range(len(s_list)):
+            if s_list[i]==b:
+                s_list[i]=c
         print(''.join(s_list))
