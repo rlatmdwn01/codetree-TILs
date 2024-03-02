a43d55b9c3bc77a9002a37c4be8236d2e141fc61
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-02 15:34:18 +0900

    Title: 문자열 돌리기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/문자열 돌리기/SPin-SPring.py
+++ 240302/문자열 돌리기/SPin-SPring.py
@@ -1,7 +1,7 @@
 word=input()
 n=len(word)
 print(word)
-new_str=word[-1]+word[:4]
+new_str=word[-1]+word[:n-1]
 for i in range(n):
     print(new_str)
-    new_str=new_str[-1]+new_str[:4]
+    new_str=new_str[-1]+new_str[:n-1]
