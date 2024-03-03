4a9de8458472cf444cdb7c2c4b048e5ccee9447f
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-03 14:59:34 +0900

    Title: 미는 횟수, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/미는 횟수/number-of-pushes.py
+++ 240303/미는 횟수/number-of-pushes.py
@@ -1,22 +1,21 @@
-def min_shift_to_match(a, b):
+# 문자열을 입력받습니다.
+a = input()
+b = input()
+	
+leng = len(a)
+cnt = 0
+	
+# 문자열 a를 우측으로 한 칸씩 밀어보면서 문자열 b와 같아지는지 확인합니다.
+for i in range(leng):
+    # 문자열을 오른쪽으로 한 칸 쉬프트합니다.
+    a = a[leng - 1] + a[:leng - 1]
+
+    cnt += 1
+    
+    # 문자열이 같을 경우 민 횟수를 출력합니다.
     if a == b:
-        return 0
-    
-    len_a = len(a)
-    len_b = len(b)
-    
-    # 문자열 A를 우측으로 한 칸씩 밀었을 때, 문자열 A와 B가 같아지는 경우를 찾음
-    for shift in range(1, len_a + 1):
-        shifted_a = a[-shift:] + a[:-shift]  # 문자열 A를 우측으로 shift 만큼 밀기
-        if shifted_a == b:
-            return shift
-    
-    return -1  # 불가능한 경우
+        print(cnt)
+        break
 
-# 입력
-string_a = input().strip()
-string_b = input().strip()
-
-# 결과 출력
-result = min_shift_to_match(string_a, string_b)
-print(result)
+    # 만약 불가능하다면 -1을 출력합니다.
+    if i == leng - 1: print("-1")
