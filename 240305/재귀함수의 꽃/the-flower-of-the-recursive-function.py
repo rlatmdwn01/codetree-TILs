78392629cc1879092d72bd22bddebc17debeb3c1
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-05 08:11:21 +0900

    Title: 재귀함수의 꽃, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240305/재귀함수의 꽃/the-flower-of-the-recursive-function.py
@@ -0,0 +1,18 @@
+n=int(input())
+
+def print_number1(n):
+    if n==0:
+        return
+    
+    print_number1(n-1)
+    print(n,end=" ")
+
+def print_number2(n):
+    if n==0:
+        return
+
+    print(n,end=" ")
+    print_number2(n-1)
+
+print_number2(n)
+print_number1(n)
