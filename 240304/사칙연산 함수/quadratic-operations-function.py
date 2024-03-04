c2ff2aff48ac04709b2fb421368aedb3c4615ef7
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-04 20:14:30 +0900

    Title: 사칙연산 함수, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240304/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,19 @@
+def calculator(i,j,k):
+    i=int(i)
+    k=int(k)
+    if j=='+':
+        return i+k
+    elif j=='-':
+        return i-k
+    elif j=='/':
+        return int(i/k)
+    elif j=='*':
+        return i*k
+    else:
+        return False
+
+a,b,c=input().split()
+if b!='+' or '-' or '/' or '*':
+    print(calculator(a,b,c))
+else:
+    print("{0} {1} {2} = {3}".format(a,b,c,calculator(a,b,c)))
