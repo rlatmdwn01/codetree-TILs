f4debfdda60d1461fb1506c3750cf1734e95fef4
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-04 22:28:03 +0900

    Title: 짝수만 2로 나누기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240304/짝수만 2로 나누기/divide-even-numbers-by-2.py
@@ -0,0 +1,11 @@
+n=int(input())
+arr=list(map(int,input().split()))
+
+def modify(arr):
+    for i in range(n):
+        if arr[i]%2==0:
+            arr[i]//=2
+
+modify(arr)
+for elem in arr:
+    print(elem, end=" ")
