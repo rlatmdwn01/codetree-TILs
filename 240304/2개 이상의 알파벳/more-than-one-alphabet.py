4251a93681f1158e0bb8fe28042b2552c803388e
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-04 22:58:45 +0900

    Title: 2개 이상의 알파벳, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240304/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,18 @@
+word=input()
+n=len(word)
+str_list=list(word)
+
+def two_alphabet(word):
+    cnt=0
+    for i in range(n-1):
+        if str_list[i]==str_list[i+1]:
+            cnt+=1
+
+    if cnt==n:
+        return False
+    return True
+
+if two_alphabet(word):
+    print('Yes')
+else:
+    print('No')
