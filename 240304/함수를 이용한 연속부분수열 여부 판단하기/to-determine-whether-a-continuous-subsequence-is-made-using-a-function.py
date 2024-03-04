dfce45f6b75590f6fdeb0806fc6167c9addb20e8
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-04 21:05:16 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 140ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
+++ 240304/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -1,15 +1,25 @@
-def get_answer(A,B):
-    for i in range(n1-n2-1):
-        for j in range(n2):
-            if A[i]==B[j]:
-                return True
-    return False
+def is_subsequence(A, B):
+    n = len(A)
+    m = len(B)
+    
+    # 수열 B의 각 원소가 수열 A에 있는지 확인
+    j = 0
+    for i in range(n):
+        if j == m:
+            break
+        if A[i] == B[j]:
+            j += 1
+    
+    # 수열 B의 모든 원소가 수열 A에 있는 경우, 수열 B는 수열 A의 연속 부분 수열이다.
+    return j == m
 
-n1,n2=tuple(map(int,input().split()))
-A=list(map(int,input().split()))
-B=list(map(int,input().split()))
+# 입력
+n1, n2 = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
 
-if get_answer(A,B):
-    print('Yes')
+# 결과 출력
+if is_subsequence(A, B):
+    print("Yes")
 else:
-    print('No')
+    print("No")
