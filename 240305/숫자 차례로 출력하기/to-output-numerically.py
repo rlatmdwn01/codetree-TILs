0db666262bdcee805e9825f9e0fe9fc20cb28285
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-05 08:04:29 +0900

    Title: 숫자 차례로 출력하기, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240305/숫자 차례로 출력하기/to-output-numerically.py
+++ 240305/숫자 차례로 출력하기/to-output-numerically.py
@@ -1,21 +1,19 @@
-def print_numbers_from_1_to_N(curr, N):
-    if curr > N:
+n=int(input())
+
+def print_number1(n):
+    if n==0:
         return
-    print(curr, end=" ")
-    print_numbers_from_1_to_N(curr + 1, N)
+    
+    print_number1(n-1)
+    print(n,end=" ")
 
-def print_numbers_from_N_to_1(curr, N):
-    if curr < 1:
+def print_number2(n):
+    if n==0:
         return
-    print(curr, end=" ")
-    print_numbers_from_N_to_1(curr - 1, N)
 
-# 입력
-N = int(input())
+    print(n,end=" ")
+    print_number2(n-1)
 
-# 첫 번째 재귀 함수 호출하여 1부터 N까지 출력
-print_numbers_from_1_to_N(1, N)
+print_number1(n)
 print()
-
-# 두 번째 재귀 함수 호출하여 N부터 1까지 출력
-print_numbers_from_N_to_1(N, N)
+print_number2(n)
