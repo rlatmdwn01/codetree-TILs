0d11c3a691b0e6afc88bb9a40b4404027ad3f823
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-08 21:58:43 +0900

    Title: 비오는 날, Time: 143ms, Memory: 25MB, Status: Passed - CodeTree

--- 240308/비오는 날/rainy-day.py
+++ 240308/비오는 날/rainy-day.py
@@ -1,18 +1,24 @@
-class WeatherData:
-    def __init__(self, date, day, weather):
-        self.date = date
-        self.day = day
-        self.weather = weather
-
-# 입력 처리
+# 변수 선언 및 입력
 n = int(input())
-weather_data_list = []
-for _ in range(n):
-    date, day, weather = input().split()
-    weather_data_list.append(WeatherData(date, day, weather))
 
-# 비가 오는 날 찾기
-for data in weather_data_list:
-    if data.weather == "Rain":
-        print(data.date, data.day, data.weather)
-        break
+
+# Forecast 정보를 나타내는 클래스 선언
+class Forecast:
+    def __init__(self, date, day, weather):
+        self.date, self.day, self.weather = date, day, weather
+
+
+ans = Forecast("9999-99-99", "", "")
+for _ in range(n):
+    date, day, weather = tuple(input().split())
+
+    # Forecast 객체를 만들어 줍니다.
+    f = Forecast(date, day, weather)
+    if weather == "Rain":
+        # 비가 오는 경우 가장 최근인지 확인하고,
+        # 가장 최근일 경우 정답을 업데이트합니다.
+        if ans.date >= f.date:
+            ans = f
+
+# 결과를 출력합니다.
+print(ans.date, ans.day, ans.weather)
