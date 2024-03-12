115c0cf0407b0476a57a0a9a0ff9975830efb0d1
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-12 22:19:38 +0900

    Title: 왔다 갔던 구역 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240312/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -2,19 +2,17 @@ def find_revisited_area(n, commands):
     position = 0
     visited = set()
     revisited = set()
-
+    
     for command in commands:
         distance, direction = command.split()
         distance = int(distance)
         
-        # 이동 방향에 따라 위치를 업데이트하고 방문한 위치를 기록합니다.
         for _ in range(distance):
             if direction == "R":
                 position += 1
             elif direction == "L":
                 position -= 1
             
-            # 현재 위치가 이미 방문한 위치라면 revisited에 추가합니다.
             if position in visited:
                 revisited.add(position)
             visited.add(position)
