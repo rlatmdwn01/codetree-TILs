43ab5a15f4da23b61c5ad4a0051fdacdd62c8db6
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-12 22:49:43 +0900

    Title: 신기한 타일 뒤집기, Time: 232ms, Memory: 31MB, Status: Passed - CodeTree

---
+++ 240312/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,41 @@
+MAX_K = 100000
+# 변수 선언 및 입력:
+n = int(input())
+a = [0] * (2 * MAX_K + 1)
+cnt_b = [0] * (2 * MAX_K + 1)
+cnt_w = [0] * (2 * MAX_K + 1)
+b, w= 0, 0
+
+cur = MAX_K
+for _ in range(n):
+    x, c = tuple(input().split())
+    x = int(x)
+
+    if c == 'L':
+        # x칸 왼쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 1
+            cnt_w[cur] += 1
+            x -= 1
+
+            if x: 
+                cur -= 1
+    else:
+        # x칸 오른쪽으로 칠합니다.
+        while x > 0:
+            a[cur] = 2
+            cnt_b[cur] += 1
+            x -= 1
+
+            if x: 
+                cur += 1
+
+for i in range(2 * MAX_K + 1):
+    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
+    if a[i] == 1: 
+        w += 1
+    elif a[i] == 2: 
+        b += 1
+
+# 정답을 출력합니다.
+print(w, b)
