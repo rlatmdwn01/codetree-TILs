50146b2e459072cf2d1940384bd92817222cfab7
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-16 18:25:25 +0900

    Title: 연속되는 수 4, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/연속되는 수 4/continuous-number4.py
+++ 240316/연속되는 수 4/continuous-number4.py
@@ -1,17 +1,14 @@
-n=int(input())
-arr=[]
-
-for _ in range(n):
-    a=int(input())
-    arr.append(a)
-
-ans,cnt=0,1
-for i in range(1,n-1):
-    if arr[i-1]<arr[i]:
-        cnt+=1
+n = int(input())
+ans = 0
+cnt = 0
+num = 0
+for i in range(n):
+    num_new = int(input())
+    if num_new > num:
+        cnt += 1
     else:
-        cnt=1
+        cnt = 1
+    ans = max(cnt, ans)
+    num = num_new
 
-    ans=max(ans,cnt)
-
-print(cnt)
+print(ans)
