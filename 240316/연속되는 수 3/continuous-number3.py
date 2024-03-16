a5bec8d40da7f0bc9341c66188090428abf1ba20
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-16 18:16:14 +0900

    Title: 연속되는 수 3, Time: 130ms, Memory: 24MB, Status: Passed - CodeTree

--- 240316/연속되는 수 3/continuous-number3.py
+++ 240316/연속되는 수 3/continuous-number3.py
@@ -1,20 +1,26 @@
+def max_sign_subarray_length(n, numbers):
+    max_length = 0
+    current_length = 0
+    prev_sign = None
+
+    for number in numbers:
+        if number == 0:
+            current_length = 0
+            prev_sign = None
+        else:
+            if prev_sign is None or (number > 0 and prev_sign > 0) or (number < 0 and prev_sign < 0):
+                current_length += 1
+                if current_length > max_length:
+                    max_length = current_length
+            else:
+                current_length = 1
+            prev_sign = number
+
+    return max_length
+
+# 입력 받기
 n = int(input())
-cnt = 0
-result = 0
-arr = []
+numbers = [int(input()) for _ in range(n)]
 
-for _ in range(n):
-    a = int(input())
-    arr.append(a)
-    
-for i in range(n):
-    if arr[i]>0:
-        cnt += 1
-    
-    else:
-        result = max(cnt, result)
-        cnt = 0
-        
-    result = max(cnt, result)
-
-print(result)
+# 결과 출력
+print(max_sign_subarray_length(n, numbers))
