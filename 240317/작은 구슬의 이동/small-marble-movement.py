fb74ec3b4ef16ce3618fa8d0517dac460f036bc6
Seung Ju Kim <69657433+rlatmdwn01@users.noreply.github.com>
2024-03-17 22:19:25 +0900

    Title: 작은 구슬의 이동, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240317/작은 구슬의 이동/small-marble-movement.py
+++ 240317/작은 구슬의 이동/small-marble-movement.py
@@ -1,27 +1,32 @@
-n,t = map(int, input().split())
-r,c,d = input().split()
-r, c = int(r), int(c)
+# 변수 선언 및 입력
+n, t = tuple(map(int, input().split()))
+x, y, c_dir = tuple(input().split())
+
+# 각 알파벳 별 방향 번호를 설정합니다.
 mapper = {
-    'R' : 0,
-    'D' : 1,
-    'U' : 2,
-    'L' : 3
+    'R': 0,
+    'D': 1,
+    'U': 2,
+    'L': 3
 }
+x, y, move_dir = int(x) - 1, int(y) - 1, mapper[c_dir]
 
-def in_range(x,y):
-    return 0<= x and x < n and 0<=y and y<n
+dxs = [0, 1, -1,  0]
+dys = [1, 0,  0, -1]
 
-def move(x,y,time,t):
-    c_dir = mapper[d]
-    dxs, dys = [0,1,-1,0],[1,0,0,-1]
-    while time < t:
-        nx,ny = x + dxs[c_dir] , y + dys[c_dir]
-        if not in_range(nx,ny):
-            c_dir = 3 - c_dir
-            time += 1
-        x,y = x + dxs[c_dir],  y + dys[c_dir]
-        time += 1
-    return x,y
 
-x,y = move(r-1,c-1,0,t)
-print(x+1,y+1)
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+
+# simulation 진행
+for _ in range(t):
+    nx, ny = x + dxs[move_dir], y + dys[move_dir]
+    # 범위 안에 들어온다면 그대로 진행합니다.
+    if in_range(nx, ny):
+        x, y = nx, ny
+    # 벽에 부딪힌다면, 방향을 바꿔줍니다.
+    else:
+        move_dir = 3 - move_dir
+
+print(x + 1, y + 1)
